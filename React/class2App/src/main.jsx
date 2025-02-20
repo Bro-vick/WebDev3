87b7aac5117e240f012ae5574b1fa5d1e6c86a9f
George Victor <georgev421@gmail.com>
2025-02-20 10:25:36 +0100

    React Begins

---
+++ React/class2App/src/main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+// import './index.css'
+import App2 from './App2'
+// import App from './App.jsx'
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    {/* <App /> */}
+    <App2 />
+  </StrictMode>,
+)
